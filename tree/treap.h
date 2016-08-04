57c67d5e7524402d4b75af8ccd47eb05b2feea4a
Bruce Kuo <bruce3557@yahoo-inc.com>
2016-08-05 01:45:49 +0800

    Add ninja namespace

--- tree/treap.h
+++ tree/treap.h
@@ -1,20 +1,31 @@
 #ifndef __TREAP_H__
 #define __TREAP_H__
 
-template <class T>
-class TreapNode {
-    private:
-        TreapNode *left;
-        TreapNode *right;
-        T value;
-        T priority;
+#include <utility>
 
-    public:
-        TreapNode(T x);
-        TreapNode* Split(TreapNode* node);
-        TreapNode* Merge(TreapNode* node);
-        TreapNode* Insert(T x);
-        TreapNode* Delete(T x);
-};
+namespace ninja {
+    template <class T>
+    class TreapNode {
+        private:
+            TreapNode *left;
+            TreapNode *right;
+            T value;
+            int priority;
+
+        public:
+            TreapNode(T x);
+            TreapNode* Insert(T x);
+            TreapNode* Delete(T x);
+            TreapNode* Query(T x);
+    };
+
+    template <class T>
+    TreapNode<T>*
+    Merge(TreapNode<T>* x, TreapNode<T>* y);
+
+    template <class T>
+    std::pair<TreapNode<T>*, TreapNode<T>*> 
+    Split(TreapNode<T>* x, T val);
+}
 
 #endif
