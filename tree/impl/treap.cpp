8d80a4b472802d41769def24c21336355f377089
Bruce Kuo <bruce3557@yahoo-inc.com>
2016-08-05 01:59:01 +0800

    Implement basic operations and merge function

---
+++ tree/impl/treap.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+
+#include "../treap.h"
+
+namespace ninja {
+    /*
+     * Implement functions in TreapNode class
+     */
+    template <class T>
+    TreapNode<T>::TreapNode(T x) {
+       value = x;
+       priority = rand();
+       left = right = NULL;
+    }
+
+    template <class T>
+    T TreapNode<T>::getValue() {
+        return value;
+    }
+
+
+    /*
+     * Implement functions required by Treap
+     */
+    template <class T>
+    TreapNode<T>* 
+    Merge(TreapNode<T>* x, TreapNode<T>* y) {
+        if (!x || !y) {
+            // return non-NULL node
+            return x ? x : y;
+        }
+
+        if (x->getValue() <= y->getValue()) {
+            x->right = Merge(x->right, y);
+        } else {
+            y->left = Merge(x, y->left);
+        }
+
+        return NULL;
+    }
+}
+
